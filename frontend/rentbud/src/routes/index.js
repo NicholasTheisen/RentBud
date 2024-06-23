e95b0a294d2aa0f5f9e382f5bd602f175fe6fe6c
NicholasTheisen <nicholas.theisen1997@gmail.com>
2024-06-23 06:58:59 +0000

    added index homepage

---
+++ frontend/rentbud/src/routes/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/HomePage.scss'; // import the new CSS file
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+      <header className="HomePage-header">
+        <h1>Rent Bud</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus.</p>
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
